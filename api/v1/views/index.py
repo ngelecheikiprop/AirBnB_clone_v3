e5644d22d63b93d82cf15294e71091801e867c94
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-08-06 06:58:52 +0000

    number 3 files

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
-""" Index """
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-from models import storage
+
 from api.v1.views import app_views
 from flask import jsonify
-
-
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status')
 def status():
-    """ Status of API """
-    return jsonify({"status": "OK"})
+    """retrun status of api
+    """
+    from flask import jsonify
+    return jsonify({"status":"ok"})
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def number_objects():
-    """ Retrieves the number of each objects by type """
-    classes = [Amenity, City, Place, Review, State, User]
-    names = ["amenities", "cities", "places", "reviews", "states", "users"]
-
-    num_objs = {}
-    for i in range(len(classes)):
-        num_objs[names[i]] = storage.count(classes[i])
-
-    return jsonify(num_objs)
+@app_views.route('/stats')
+def stats():
+    """get the numbers of each object
+    """
+    from models import storage
+    from models.amenity import Amenity
+    from models.state import State
+    from models.review import Review
+    from models.city import City
+    from models.user import User
+    from models.place import Place
+    mystats = {}
+    mystats["amenities"] = storage.count(Amenity)
+    mystats["cities"] = storage.count(City)
+    mystats["places"] = storage.count(Place)
+    mystats["reviews"] = storage.count(Review)
+    mystats["states"] = storage.count(State)
+    mystats["users"] = storage.count(User)
+    return jsonify(mystats)
